da28305cf99490098f2f396d115761f8d92e3bc8
Tony Carlson <carlson.tony@gmail.com>
2023-07-05 16:25:43 -0500

    Add maptorow method

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,11 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+
+import java.util.List;
+
 public interface AccountDao {
+
+    double getAccountBalance();
+    List<Account> getAccounts();
 }
