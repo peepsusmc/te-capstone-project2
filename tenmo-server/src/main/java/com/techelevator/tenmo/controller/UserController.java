0b443e0917d329c538708039ef9da021bfa7501f
Christopher Peoples <peepsusmc@gmail.com>
2023-07-06 13:39:03 -0500

    User list works

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 public class UserController {
-    
+
+    private final UserDao userDao;
+
+    public UserController(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> findAll(){
+        List<User> users = userDao.findAll();
+        return users;
+    }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -3,10 +3,10 @@ package com.techelevator.tenmo.dao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-@Component
-public class JdbcTransferDao implements TransferDao{
-    private final JdbcTemplate jdbcTemplate;
-    public JdbcTransferDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-}
+//@Component
+//public class JdbcTransferDao implements TransferDao{
+//    private final JdbcTemplate jdbcTemplate;
+//    public JdbcTransferDao(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//}
